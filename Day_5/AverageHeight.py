4dd45c67fc79d3e4ce6156e945ddda895bcb1f56
lythanhhai <you@example.com>
2022-02-27 15:26:17 +0700

    completely day 5 DHBK_Python

---
+++ Day_5/AverageHeight.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
+sum = 0
+for height in student_heights:
+    sum += height
+
+print("Average height is: " + str(sum / len(student_heights))) 

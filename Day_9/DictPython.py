b2a13edfc4cfff2aae9357506b2e5422f9c04321
lythanhhai <you@example.com>
2022-03-03 09:17:20 +0700

    completely exercise day 9

---
+++ Day_9/DictPython.py
@@ -0,0 +1,12 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+programming_dictionary["loop"] = "The action of doing something over and over again"
+
+empty_dict = {}
+
+#programming_dictionary = {}
+programming_dictionary["Bug"] = "hello "
+print(programming_dictionary["Bug"])
+
+for key in programming_dictionary:
+    print(key)

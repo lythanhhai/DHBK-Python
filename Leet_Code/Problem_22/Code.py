8810e7e5a43fdb482f9b5a195a1c3582aeda3a3a
lythanhhai <you@example.com>
2022-03-25 22:21:14 +0700

    :ok_hand: completely 2 problem and solution in leetcode

---
+++ Leet_Code/Problem_22/Code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        count = 0
+        perimeter = 0
+        for row in range(0, len(grid)):
+            for col in range(0, len(grid[0])):
+                if grid[row][col] == 1:
+                    perimeter += 4;
+                    if(row > 0 and grid[row - 1][col] == 1):
+                        perimeter -= 2
+                    if(col > 0 and grid[row][col - 1] == 1):
+                        perimeter -= 2
+        return perimeter
